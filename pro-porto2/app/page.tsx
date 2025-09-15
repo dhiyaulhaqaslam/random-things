2e8ea5e8a59d666ebef5637f4157b317715e26f2
Dhiyaulhaq Aslam <dhias313@gmail.com>
2025-09-15 10:45:59 +0800

    page.tsx adjustment

--- pro-porto2/app/page.tsx
+++ pro-porto2/app/page.tsx
@@ -6,6 +6,18 @@ export default function Home() {
          <div>
             <h2 className="bg-red-800 text-xs lowercase">IKLAN DISINI</h2>
          </div>
+         <div>
+            <h2>INI HERO</h2>
+         </div>
+         <div>
+            <h2>INI ABOUT</h2>
+         </div>
+         <div>
+            <h2>INI PROJECTS</h2>
+         </div>
+         <div>
+            <h2>INI CONTACT</h2>
+         </div>
       </>
    );
 }
