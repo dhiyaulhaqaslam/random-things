6d515f5320c66f102ee08c4c853f2cf78a04161a
Dhiyaulhaq Aslam <dhias313@gmail.com>
2025-09-15 10:44:31 +0800

    new project pro-porto2

---
+++ pro-porto2/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+   return (
+      <>
+         <div>
+            <h2 className="bg-red-800 text-xs lowercase">IKLAN DISINI</h2>
+         </div>
+      </>
+   );
+}
